d7515f4d293ae4e34740189400cdec80f81300af
Usha Mayuri <usha@bebetta.in>
2024-07-19 21:55:53 +0530

    Initial Commit

---
+++ Swipe Assignment/Swipe_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_AssignmentApp.swift
+//  Swipe Assignment
+//
+//  Created by usha mayuri on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
